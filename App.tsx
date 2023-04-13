797ebdfa4cd8cc80b9e117105deb7c489f19fae9
Juan Sánchez <jlsc92@gmail.com>
2023-04-13 17:48:03 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -3,17 +3,24 @@ import {SafeAreaView} from 'react-native';
 /* import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 import {CounterScreen} from './src/screens/CounterScreen';
 import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
-import {DimensionesScreen} from './src/screens/DimensionesScreen'; */
+import {DimensionesScreen} from './src/screens/DimensionesScreen';
 import {PositionScreen} from './src/screens/PositionScreen';
+import {FlexScreen} from './src/screens/FlexScreen'; */
+import {TareaScreen} from './src/screens/TareaScreen';
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}>
       {/* <HolaMundoScreen />
       <CounterScreen />
       <BoxObjectModelScreen />
-      <DimensionesScreen /> */}
+      <DimensionesScreen />
       <PositionScreen />
+      <FlexScreen /> */}
+      <TareaScreen />
     </SafeAreaView>
   );
 };
